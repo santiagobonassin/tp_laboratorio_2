208f99383bf0389027937b465b68aaa59d649182
santiagobonassin <tomasangelo8@gmail.com>
2017-11-13 19:15:51 -0300

    Trabajo Practico 4

---
+++ TP 4 Santiago Bonassin/Archivos/Texto.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Archivos
+{
+    public class Texto : IArchivo<string>
+    {
+        string nombreArchivo;
+
+        public Texto(string archivo)
+        {
+            this.nombreArchivo = archivo;
+        }
+        public bool guardar(string datos)
+        {
+            try
+            {
+                using (StreamWriter sw=new StreamWriter(this.nombreArchivo,true))
+                {
+                    sw.WriteLine(datos);
+                }
+                return true;
+            }
+            catch(Exception e)
+            {
+                return false;
+            }
+        }
+        public bool leer(out List<string>datos)
+        {
+            datos = new List<string>();
+            try
+            {
+                using (StreamReader sr = new StreamReader(this.nombreArchivo))
+                {
+                    while(!sr.EndOfStream)
+                    {
+                        datos.Add(sr.ReadLine());
+                    }                   
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ TP 4 Santiago Bonassin/Hilo/Descargador.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net; // Avisar del espacio de nombre
+using System.ComponentModel;
+
+namespace Hilo
+{
+    public class Descargador
+    {
+        private string html;
+        private Uri direccion;
+        public Descargador(Uri direccion)
+        {
+            this.html = "";
+            this.direccion = direccion;
+        }
+        public void IniciarDescarga()
+        {
+            try
+            {
+                WebClient cliente = new WebClient();
+                cliente.DownloadProgressChanged += new DownloadProgressChangedEventHandler(this.WebClientDownloadProgressChanged);
+                cliente.DownloadStringCompleted += new DownloadStringCompletedEventHandler(this.WebClientDownloadCompleted);
+                cliente.DownloadStringAsync(direccion);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+        private void WebClientDownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+        }
+        private void WebClientDownloadCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            this.html = e.Result;
+        }
+    }
+}
