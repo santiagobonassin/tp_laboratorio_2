7e7981e4fe314fd10c2e52a991732b99da6cbc02
santiagobonassin <tomasangelo8@gmail.com>
2017-11-14 15:13:54 -0300

    Add files via upload

---
+++ TP 4 Santiago Bonassin(terminado)/Hilo/Descargador.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net; // Avisar del espacio de nombre
+using System.ComponentModel;
+
+namespace Hilo
+{
+    public class Descargador
+    {
+        private string html;
+        private Uri direccion;
+        public Descargador(Uri direccion)
+        {
+            this.html = "";
+            this.direccion = direccion;
+        }
+        public void IniciarDescarga()
+        {
+            try
+            {
+                WebClient cliente = new WebClient();
+                cliente.DownloadProgressChanged += new DownloadProgressChangedEventHandler(WebClientDownloadProgressChanged);
+                cliente.DownloadStringCompleted += new DownloadStringCompletedEventHandler(WebClientDownloadCompleted);
+                cliente.DownloadStringAsync(direccion);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+        private void WebClientDownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            this.Progreso(e.ProgressPercentage);
+        }
+        private void WebClientDownloadCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {          
+            html = e.Result;
+            EventoFinalizado(html);
+        }
+        public delegate void EventProgress(int progreso);
+        public delegate void End(string html);
+        public event EventProgress Progreso;
+        public event End EventoFinalizado;
+    }
+}
---
+++ TP 4 Santiago Bonassin(terminado)/Navegador/frmHistorial.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Navegador
+{
+    public partial class frmHistorial : Form
+    {
+        public const string ARCHIVO_HISTORIAL = "Historial.dat";
+
+        public frmHistorial()
+        {
+            InitializeComponent();
+        }
+        private void frmHistorial_Load(object sender, EventArgs e)
+        {
+            Archivos.Texto archivos = new Archivos.Texto(ARCHIVO_HISTORIAL);
+            List<string> ListaDePaginas = new List<string>();
+            if (!archivos.leer(out ListaDePaginas))
+            {
+                this.Close();
+            }
+            else
+            {
+                foreach (string i in ListaDePaginas)
+                {
+                    this.lstHistorial.Items.Add(i);
+                }
+            }
+        }
+        private void lstHistorial_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
